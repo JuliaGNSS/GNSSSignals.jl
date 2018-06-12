948d44095c7763d86b90298bc20f75fc279d0ac1
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-12 17:49:21 +0200

    change tests

--- test/gpsl5.jl
+++ test/gpsl5.jl
@@ -1,13 +1,13 @@
 @testset "GPS L5" begin
-gen_sampled_code, get_code_phase = @inferred GNSSSignals.init_gpsl5_codes(1)
+gen_sampled_code, get_code_phase = @inferred GNSSSignals.init_gpsl5_code()
 
-code = @inferred gen_sampled_code(0:10229, 10230, 0, 10230)
+code = @inferred gen_sampled_code(0:10229, 10230, 0, 10230, 1)
 power = code' * code / 10230
-@test power ≈ 1
+@test power ≈ 1 atol = 1e-4
 @test code == L5_SAT1_I5_Code
 
-early = @inferred gen_sampled_code(1:40920, 1023e4, 3.5, 4 * 1023e4)
-prompt = @inferred gen_sampled_code(1:40920, 1023e4, 4, 4 * 1023e4)
-late = @inferred gen_sampled_code(1:40920, 1023e4, 4.5, 4 * 1023e4)
+early = @inferred gen_sampled_code(1:40920, 1023e4, 3.5, 4 * 1023e4, 2)
+prompt = @inferred gen_sampled_code(1:40920, 1023e4, 4, 4 * 1023e4, 2)
+late = @inferred gen_sampled_code(1:40920, 1023e4, 4.5, 4 * 1023e4, 2)
 @test early' * prompt == late' * prompt
 end
