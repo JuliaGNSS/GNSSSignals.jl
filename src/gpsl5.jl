74a2d8b8c177ace2da8154ed7fb9918fca427a57
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-20 14:41:58 +0200

    get rid of slower and therefore unnecessary functions; add Docu; rename functions

--- src/gpsl5.jl
+++ src/gpsl5.jl
@@ -1,4 +1,14 @@
-function darstellen(integer)
+"""
+$(SIGNATURES)
+
+Takes the status of the registers as an `integer` and returns them as an array.
+# Examples
+```julia-repl
+julia> reshape(8190)
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+```
+"""
+function reshape(integer)
     a = zeros(13)
     for i = 1:13
         b = (integer >> (i-1)) & 1
@@ -7,14 +17,22 @@ function darstellen(integer)
     a
 end
 
-function shift_register_mit_uebergabe(register, update_func)
-    output = register[13]
-    update = update_func(register)
-    unshift!(register, update)
-    output, register 
-end
+"""
+$(SIGNATURES)
 
-function shift_register_int_freitag(register, indices)
+Takes the status of the registers as an Int `registers`, and an array of register `indices` to calculate and return the new register values and the register_output.
+
+```julia-repl
+julia> reshape(8190)
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+julia> output, registers = shift_register(8910,  [9, 10, 12, 13])
+julia> reshape(registers)
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+julia> output == 1
+true
+```
+"""
+function shift_register(register, indices)
     update = 0
     for i in indices
         update = update ⊻ ((register >> (13 - i)) & 1)
@@ -22,15 +40,24 @@ function shift_register_int_freitag(register, indices)
     (register & 1), (register >> 1) + 2^12 * update
 end
 
-function gen_L5_I5_code_with_ints_freitag(initial_xb_code_states)
+"""
+$(SIGNATURES)
+
+Calculate the gps L5 PRN `satellite_code` for the initial XB register states 'initial_xb_code_states'.
+```julia-repl
+julia> initial_states_PRN_num_1_I = [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0]
+julia> prn_code_sat_1_I_signal = gen_l5_code(initial_states_PRN_num_1_I)
+```
+"""
+function gen_l5_code(initial_xb_code_states)
     XA = 8191 # int with 3 leading zeros and then 13*1
     XB = initial_xb_code_states' * [4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]
     satellite_code = zeros(Int8, 10230)
-    XA_indices = [9,10,12,13]
-    XB_indices = [1,3,4,6,7,8,12,13]
+    XA_indices = [9, 10, 12, 13]
+    XB_indices = [1, 3, 4, 6, 7, 8, 12, 13]
     for i = 1:10230
-        output_xa, XA = shift_register_int_freitag(XA, XA_indices)
-        output_xb, XB = shift_register_int_freitag(XA, XB_indices)
+        output_xa, XA = shift_register(XA, XA_indices)
+        output_xb, XB = shift_register(XB, XB_indices)
         satellite_code[i] =  2 * (output_xa ⊻ output_xb) - 1
         if (i == 8190)
             XA = 8191
@@ -39,84 +66,18 @@ function gen_L5_I5_code_with_ints_freitag(initial_xb_code_states)
     return satellite_code
 end
 
-function gen_L5_I5_code_with_param_and_circular_buffer(initial_xb_code_states)
-    XA = CircularBuffer{Int}(13)
-    XB = CircularBuffer{Int}(13)
-    append!(XA, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    append!(XB, initial_xb_code_states)
-    satellite_code = zeros(Int8, 10230)
-    for i = 1:10230
-        output_xa, XA = shift_register_mit_uebergabe(XA, XA -> XA[9] ⊻ XA[10] ⊻ XA[12] ⊻ XA[13])
-        output_xb, XB = shift_register_mit_uebergabe(XB, XB -> XB[1] ⊻ XB[3] ⊻ XB[4] ⊻ XB[6] ⊻ XB[7] ⊻ XB[8] ⊻ XB[12] ⊻ XB[13])
-        satellite_code[i] =  2* (output_xa ⊻ output_xb) - 1
-        if (i == 8190)
-            append!(XA, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        end
-        
-    end
-    satellite_code
-end
-
-"""
-Generate L5 PRN satellite code withe the `initial_xb_code_states`.
-# Examples
-```julia-repl
-julia> gen_sat_code(1:4000, 1023e3, 2, 4e6, [1, -1, 1, 1, 1])
-```
-"""
-
-function gen_L5_I5_code(initial_xb_code_states)
-    XA = CircularBuffer{Int}(13)
-    XB = CircularBuffer{Int}(13)
-    satellite_code = zeros(Int8, 10230)
-    append!(XA, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    append!(XB, initial_xb_code_states)
-    for i = 1:10230
-        xa = XA[9] + XA[10] + XA[12] + XA[13]
-        xb = XB[1] + XB[3] + XB[4] + XB[6] + XB[7] + XB[8] + XB[12] + XB[13]
-        output = (XA[13] + XB[13]) % 2
-        unshift!(XA, xa % 2)
-        unshift!(XB, xb % 2)
-        satellite_code[i] = 2 * output - 1
-        if (i == 8190)
-            append!(XA, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        end
-    end
-    return satellite_code
-end
-
-function gen_L5_I5_code_bool_without_extern_func(initial_xb_code_states)
-    XA = BitArray([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    XB = BitArray(initial_xb_code_states)
-    satellite_code = falses(10230)
-    for i = 1:10230
-        xa = XA[9] ⊻ XA[10] ⊻ XA[12] ⊻ XA[13]
-        xb = XB[1] ⊻ XB[3] ⊻ XB[4] ⊻ XB[6] ⊻ XB[7] ⊻ XB[8] ⊻ XB[12] ⊻ XB[13]
-        output = (XA[13] ⊻ XB[13])
-        XA >> 1
-        XA[1] = xa
-        XB >> 1
-        XB[1] = xb
-        satellite_code[i] = 2 * output - 1
-        if (i == 8190)
-            XA = BitArray([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        end
-    end
-    return satellite_code
-end
-
 """
 $(SIGNATURES)
 
-Returns functions to generate sampled code and code phase for the GPS L1, signal.
+Returns functions to generate sampled code and code phase for the GPS L5 (I5) signal.
 # Examples
 ```julia-repl
-julia> gen_gpsl1_code, get_gpsl1_code_phase = init_gpsl1_codes()
-julia> gen_gpsl1_code(samples, f, φ₀, f_s, sat)
-julia> get_code_phase(sample, f, φ₀, f_s)
+julia> gen_gpsi5_code, get_i5_code_phase = init_gpsl5_i5_codes()
+julia> gen_gpsi5_code(samples, f, φ₀, f_s, sat)
+julia> get_i5_code_phase(sample, f, φ₀, f_s)
 ```
 """
-function init_gpsl5_code()
+function init_gpsl5_i5_code()
     code_length = 10230
     #=These are the initial XB Code States for the I5 code, initial_xb_code_states[1] is a 1 3 chip array which represent the shift register values
     initial_xb_code_states[3][4] represents the 4th shift register of the GPS Signal with PRN numver 3  =#
@@ -161,9 +122,9 @@ function init_gpsl5_code()
     ]
     codes = zeros(37*code_length)
     for i = 1:37
-        codes[(code_length * (i-1) + 1):(code_length * i)] = gen_L5_I5_code(initial_xb_code_states[i])
+        codes[(code_length * (i-1) + 1):(code_length * i)] = gen_l5_code(initial_xb_code_states[i])
     end
-    gen_sampled_code(samples, f, φ₀, f_s, sat) = gen_sat_code(samples, f, φ₀, f_s, codes[(code_length * (sat-1) + 1):(code_length * sat)])
+    gen_sampled_code(samples, f, φ₀, f_s, sat) = gen_sat_code(samples, f, φ₀, f_s, codes[(code_length * (sat - 1) + 1):(code_length * sat)])
     get_sampled_code_phase(sample, f, φ₀, f_s) = get_sat_code_phase(sample, f, φ₀, f_s, code_length)
     gen_sampled_code, get_sampled_code_phase
 end
--- test/gpsl5.jl
+++ test/gpsl5.jl
@@ -1,21 +1,7 @@
 @testset "shift_registers" begin
-XB = CircularBuffer{Int}(13)
-append!(XB, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-registers = 0
-    for i = 1:8191
-        output_xb, registers = GNSSSignals.shift_register_mit_uebergabe(XB, XB -> XB[1] ⊻ XB[3] ⊻ XB[4] ⊻ XB[6] ⊻ XB[7] ⊻ XB[8] ⊻ XB[12] ⊻ XB[13])
-        if (i in [266, 804, 1559, 3471, 5343])
-            @test registers in [ [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0] , [0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0], [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1], [1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1] ]
-        end
-    end
-    @test registers == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-end
-
-@testset "shift_registers_int" begin
 registers = 8191
 for i = 1:8191
-    output_xb, registers = GNSSSignals.shift_register_int_freitag(registers, [1,3,4,6,7,8,12,13])
+    output_xb, registers = GNSSSignals.shift_register(registers, [1, 3, 4, 6, 7, 8, 12, 13])
     results = [2788, 2056 , 3322, 2087, 6431]
     if (i in [266, 804, 1559, 3471, 5343])
         @test registers in results
@@ -25,15 +11,18 @@ end
 end
 
 @testset "GPS L5" begin
-gen_sampled_code, get_code_phase = @inferred GNSSSignals.init_gpsl5_code()
+gen_sampled_code, get_code_phase = @inferred GNSSSignals.init_gpsl5_i5_code()
 
 code = @inferred gen_sampled_code(0:10229, 10230, 0, 10230, 1)
 power = code' * code / 10230
-@test power ≈ 1 atol = 1e-4
+@test power == 1 
 @test code == L5_SAT1_I5_Code
 
 early = @inferred gen_sampled_code(1:40920, 1023e4, 3.5, 4 * 1023e4, 2)
 prompt = @inferred gen_sampled_code(1:40920, 1023e4, 4, 4 * 1023e4, 2)
 late = @inferred gen_sampled_code(1:40920, 1023e4, 4.5, 4 * 1023e4, 2)
 @test early' * prompt == late' * prompt
+println("early", early[1:10])
+println("promtp", prompt[1:10])
+println("late", late[1:10])
 end
