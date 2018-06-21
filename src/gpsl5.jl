21463e0bd0b5862e79268b0fd0a5cd7c0687268f
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-21 16:47:41 +0200

    rename and adjust alignments; add neumann code generation and adjust tests; use Int8 arrays for the gpsl1 codes

--- src/gpsl5.jl
+++ src/gpsl5.jl
@@ -1,3 +1,45 @@
+#=These are the initial XB Code States for the I5 code, initial_xb_code_states[1] is a 1 3 chip array which represent the shift register values
+initial_xb_code_states[3][4] represents the 4th shift register of the GPS Signal with PRN numver 3  =#
+INITIAL_XB_CODE_STATES = [                      #sat PRN number
+        [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0],    #01
+        [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],    #02
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],    #03
+        [1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0],    #04
+        [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1],    #05
+        [0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0],    #06
+        [1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1],    #07
+        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0],    #08
+        [1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1],    #09
+        [0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],    #10
+        [0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0],    #11
+        [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1],    #12
+        [0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],    #13
+        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1],    #14
+        [0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0],    #15
+        [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],    #16
+        [0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1],    #17
+        [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0],    #18
+        [1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1],    #19
+        [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1],    #20
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],    #21
+        [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],    #22
+        [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],    #23
+        [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0],    #24
+        [1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1],    #25
+        [1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0],    #26
+        [0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],    #27
+        [0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],    #28
+        [0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],    #29
+        [1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1],    #30
+        [0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0],    #31
+        [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1],    #32
+        [1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],    #33
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],    #34
+        [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0],    #35
+        [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0],    #36
+        [0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0]     #37
+    ]
+
 """
 $(SIGNATURES)
 
@@ -77,54 +119,22 @@ julia> gen_gpsi5_code(samples, f, φ₀, f_s, sat)
 julia> get_i5_code_phase(sample, f, φ₀, f_s)
 ```
 """
+
+function gen_neuman_hofman_sequence(initial_xb_code_states)
+    satellite_code = gen_l5_code(initial_xb_code_states) # = satellite_code .⊻ 0
+    ones_sat_code = 1 .⊻ satellite_code
+    # 10 digit NH code 0000110101
+    nh_satellite_code = vcat(satellite_code, satellite_code, satellite_code, satellite_code, #0000
+     ones_sat_code, ones_sat_code, satellite_code,  #110
+      ones_sat_code, satellite_code, ones_sat_code) #101
+
+end
+
 function init_gpsl5_code()
-    code_length = 10230
-    #=These are the initial XB Code States for the I5 code, initial_xb_code_states[1] is a 1 3 chip array which represent the shift register values
-    initial_xb_code_states[3][4] represents the 4th shift register of the GPS Signal with PRN numver 3  =#
-    initial_xb_code_states = [                      #sat PRN number
-        [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0],    #01
-        [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],    #02
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],    #03
-        [1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0],    #04
-        [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1],    #05
-        [0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0],    #06
-        [1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1],    #07
-        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0],    #08
-        [1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1],    #09
-        [0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0],    #10
-        [0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0],    #11
-        [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1],    #12
-        [0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0],    #13
-        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1],    #14
-        [0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0],    #15
-        [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],    #16
-        [0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1],    #17
-        [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0],    #18
-        [1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1],    #19
-        [0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1],    #20
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],    #21
-        [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],    #22
-        [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],    #23
-        [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0],    #24
-        [1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1],    #25
-        [1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0],    #26
-        [0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],    #27
-        [0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],    #28
-        [0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],    #29
-        [1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1],    #30
-        [0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0],    #31
-        [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1],    #32
-        [1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],    #33
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],    #34
-        [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0],    #35
-        [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0],    #36
-        [0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0]     #37
-    ]
-    codes = zeros(37*code_length)
-    for i = 1:37
-        codes[(code_length * (i-1) + 1):(code_length * i)] = gen_l5_code(initial_xb_code_states[i])
-    end
-    gen_sampled_code(samples, f, φ₀, f_s, sat) = gen_sat_code(samples, f, φ₀, f_s, codes[(code_length * (sat - 1) + 1):(code_length * sat)])
+    code_length = 102300
+    codes = zeros(Int8, 102300*37)
+    codes = mapreduce(sat -> gen_neuman_hofman_sequence(INITIAL_XB_CODE_STATES[sat]), hcat, 1:37)
+    gen_sampled_code(samples, f, φ₀, f_s, sat) = gen_sat_code(samples, f, φ₀, f_s, codes[:,sat])
     get_sampled_code_phase(sample, f, φ₀, f_s) = get_sat_code_phase(sample, f, φ₀, f_s, code_length)
     gen_sampled_code, get_sampled_code_phase
 end
--- test/gpsl5.jl
+++ test/gpsl5.jl
@@ -1,28 +1,25 @@
 @testset "shift_registers" begin
-registers = 8191
-for i = 1:8191
-    output_xb, registers = GNSSSignals.shift_register(registers, [1, 3, 4, 6, 7, 8, 12, 13])
-    results = [2788, 2056 , 3322, 2087, 6431]
-    if (i in [266, 804, 1559, 3471, 5343])
-        @test registers in results
+    registers = 8191
+    for i = 1:8191
+        output_xb, registers = @inferred GNSSSignals.shift_register(registers, [1, 3, 4, 6, 7, 8, 12, 13])
+        results = [2788, 2056 , 3322, 2087, 6431]
+        if (i in [266, 804, 1559, 3471, 5343])
+            @test registers in results
+        end
     end
+    @test registers == 8191
 end
-@test registers == 8191
-end
 
-@testset "GPS L5" begin
-gen_sampled_code, get_code_phase = @inferred GNSSSignals.init_gpsl5_code()
+@testset "GPS L5 " begin
+    gen_sampled_code, get_code_phase =  GNSSSignals.init_gpsl5_code()
+    power = 0.0
+    code =  @inferred gen_sampled_code(0:10229, 10230, 0, 10230, 1)
+    power = code' * code / 10230
+    @test power == 1 
+    @test code == L5_SAT1_Code
 
-code = @inferred gen_sampled_code(0:10229, 10230, 0, 10230, 1)
-power = code' * code / 10230
-@test power == 1 
-@test code == L5_SAT1_I5_Code
-
-early = @inferred gen_sampled_code(1:40920, 1023e4, 3.5, 4 * 1023e4, 2)
-prompt = @inferred gen_sampled_code(1:40920, 1023e4, 4, 4 * 1023e4, 2)
-late = @inferred gen_sampled_code(1:40920, 1023e4, 4.5, 4 * 1023e4, 2)
-@test early' * prompt == late' * prompt
-println("early", early[1:10])
-println("promtp", prompt[1:10])
-println("late", late[1:10])
+    early = @inferred gen_sampled_code(1:40920, 1023e4, 3.5, 4 * 1023e4, 2)
+    prompt = @inferred gen_sampled_code(1:40920, 1023e4, 4, 4 * 1023e4, 2)
+    late = @inferred gen_sampled_code(1:40920, 1023e4, 4.5, 4 * 1023e4, 2)
+    @test early' * prompt == late' * prompt
 end
