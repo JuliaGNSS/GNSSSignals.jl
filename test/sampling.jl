fade75be65cdbcfe0a86be6188938bf4a285cd30
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-08-21 13:40:03 +0200

    Some Beautifying

--- test/sampling.jl
+++ test/sampling.jl
@@ -1,14 +1,14 @@
 @testset "Carrier" begin
-carrier = @inferred GNSSSignals.gen_carrier(1:1000, 1e3, 30 * pi / 180, 4e6)
-power = 1e-3 * carrier' * carrier
-@test power ≈ 1
+    carrier = @inferred GNSSSignals.gen_carrier(1:1000, 1e3, 30π / 180, 4e6)
+    power = 1e-3 * carrier' * carrier
+    @test power ≈ 1
 
-@test carrier ≈ cis.((2 * π * 1e3 / 4e6) .* (1:1000) .+ (30 * pi / 180))
+    @test carrier ≈ cis.((2 * π * 1e3 / 4e6) .* (1:1000) .+ (30π / 180))
 end
 
 @testset "Subcarrier" begin
-prn = [1 1 -1 1 -1 -1 1 1 1 -1 1 -1 -1 -1]
-code = @inferred GNSSSignals.gen_sat_code(1:1000, 1e6, 4e-7, 4e6, prn)
-power = 1e-3 * code' * code
-@test power ≈ 1
+    prn = [1 1 -1 1 -1 -1 1 1 1 -1 1 -1 -1 -1]
+    code = @inferred GNSSSignals.gen_sat_code(1:1000, 1e6, 4e-7, 4e6, prn)
+    power = 1e-3 * code' * code
+    @test power ≈ 1
 end
