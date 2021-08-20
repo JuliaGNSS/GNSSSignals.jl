636c9883b4df977795ced139612e0efb9b5d21b1
Michael Niestroj <michael.niestroj@nav.rwth-aachen.de>
2021-08-20 13:25:37 +0200

    Add tests for code spectra

--- test/common.jl
+++ test/common.jl
@@ -1,3 +1,17 @@
 @testset "Common" begin
     @test get_code_center_frequency_ratio(GPSL1()) ≈ 1/1540
 end
+
+@testset "Code spectra $(system)" for system = [GPSL1(), GPSL5(), BOCcos(GPSL1(),0,15)]
+    @test get_code_spectrum(system, 0) ≈ 1.0Hz/get_code_frequency(system)
+    @testset "Test $(m). zero" for m = 1:10
+        @test get_code_spectrum(system, m*get_code_frequency(system)) == 0
+        @test get_code_spectrum(system, -m*get_code_frequency(system)) == 0
+    end
+    @test sum(get_code_spectrum.(system, -1e12:1e4:1e12))*1e4 ≈ 1 rtol = 1e-5
+end
+
+@testset "Code spectra $(system)" for system = [GalileoE1B(), BOCcos(GPSL1(),15,2.5)]
+    @test get_code_spectrum(system, 0) == 0.0
+    @test sum(get_code_spectrum.(system, -1e12:1e4:1e12))*1e4 ≈ 1 rtol = 1e-5
+end
