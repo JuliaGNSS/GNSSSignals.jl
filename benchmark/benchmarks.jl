d08e9f0fadc39a956038a90a40cd678194648ea5
Soeren Schoenbrod <soeren.schoenbrod@juliahub.com>
2025-05-21 16:44:41 +0200

    test: update benchmark script
    Add more tests for code generation.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,13 +3,19 @@ using GNSSSignals
 using Unitful: Hz
 
 const SUITE = BenchmarkGroup()
-SUITE["code"] = BenchmarkGroup(["Code"])
 
 num_samples = 2000
-gpsl1 = GPSL1()
-sampling_frequency = 4e6Hz
-sampled_code = zeros(Int32, num_samples)
+sampled_code = zeros(Int16, num_samples)
 
-SUITE["code"]["sampling"] =
-    @benchmarkable gen_code!($sampled_code, $gpsl1, $1, $sampling_frequency) evals = 10 samples =
+SUITE["code"]["code generation"]["GPSL1"] =
+    @benchmarkable gen_code!($sampled_code, $(GPSL1()), $1, $(2e6Hz)) evals = 10 samples =
+        10000
+
+SUITE["code"]["code generation"]["GPSL5"] =
+    @benchmarkable gen_code!($sampled_code, $(GPSL5()), $1, $(20e6Hz)) evals = 10 samples =
+        10000
+
+sampled_code_f32 = zeros(Float32, num_samples)
+SUITE["code"]["code generation"]["GalileoE1B"] =
+    @benchmarkable gen_code!($sampled_code_f32, $(GalileoE1B()), $1, $(10e6Hz)) evals = 10 samples =
         10000
