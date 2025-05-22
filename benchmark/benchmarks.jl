bd10ff8f470e72edce05b4fa67a3dc61b57f3af2
Soeren Schoenbrod <soeren.schoenbrod@outlook.de>
2025-05-22 07:46:36 +0200

    test: pass maximum sampling frequency as val to benchmark (#34)
    By providing the maximum sampling frequency as Val to code
    generation, the performance will be even faster.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,15 +7,33 @@ const SUITE = BenchmarkGroup()
 num_samples = 2000
 sampled_code = zeros(Int16, num_samples)
 
-SUITE["code"]["code generation"]["GPSL1"] =
-    @benchmarkable gen_code!($sampled_code, $(GPSL1()), $1, $(2e6Hz)) evals = 10 samples =
-        10000
+SUITE["code"]["code generation"]["GPSL1"] = @benchmarkable gen_code!(
+    $sampled_code,
+    $(GPSL1()),
+    $1,
+    $(2e6Hz),
+    $0.0,
+    $0,
+    $(Val(2e6Hz)),
+) evals = 10 samples = 10000
 
-SUITE["code"]["code generation"]["GPSL5"] =
-    @benchmarkable gen_code!($sampled_code, $(GPSL5()), $1, $(20e6Hz)) evals = 10 samples =
-        10000
+SUITE["code"]["code generation"]["GPSL5"] = @benchmarkable gen_code!(
+    $sampled_code,
+    $(GPSL5()),
+    $1,
+    $(20e6Hz),
+    $0.0,
+    $0,
+    $(Val(20e6Hz)),
+) evals = 10 samples = 10000
 
 sampled_code_f32 = zeros(Float32, num_samples)
-SUITE["code"]["code generation"]["GalileoE1B"] =
-    @benchmarkable gen_code!($sampled_code_f32, $(GalileoE1B()), $1, $(15e6Hz)) evals = 10 samples =
-        10000
+SUITE["code"]["code generation"]["GalileoE1B"] = @benchmarkable gen_code!(
+    $sampled_code_f32,
+    $(GalileoE1B()),
+    $1,
+    $(15e6Hz),
+    $0.0,
+    $0,
+    $(Val(15e6Hz)),
+) evals = 10 samples = 10000
